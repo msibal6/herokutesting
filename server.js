dff637702538edd91ca41bb03fc9c3a666a7ce7c
Mitchell Sibal <mitchell.sibal@gmail.com>
2020-07-10 09:56:30 -0700

    Database testing

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express')
 var app = express();
 
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));

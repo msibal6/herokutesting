aaf7734217d5cf3891d13fca15e8b8edacbf8ee8
Mitchell Sibal <mitchell.sibal@gmail.com>
2020-07-10 11:51:11 -0700

    First connection to the database

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 var mysql = require('mysql');
@@ -6,10 +6,10 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+connection.query('SELECT * FROM v6mbnziqjbmo11km.events', function(err, rows, fields) {
   if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
+  console.log('The solution is: ', rows[0].description);
 });
 
 connection.end();

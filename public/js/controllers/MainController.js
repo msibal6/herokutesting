035b590d2b626f00cabf57e758b3b56f3dabb4da
Mitchell Sibal <mitchell.sibal@gmail.com>
2020-07-14 10:20:13 -0700

    Added simple universal authentication

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -10,9 +10,12 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
   	}
 
     function hasOnlyNumbers(item) {
-      return /^[0-9]*$/.test(item)
+      return /^[0-9]*$/.test(item);
     }
   	
+    $scope.Log = function() {
+      console.log("Tried to log in");
+    }
     $scope.addItem = function(itemList, item) {
       // ISBN : 10 or 13 length and consisdt of only numbers
       if ((item.length == 10 || item.length == 13) && hasOnlyNumbers(item)) {
@@ -21,7 +24,7 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
           itemList.push("Title: " + data.items[0].volumeInfo.title + " // Author(s):" + data.items[0].volumeInfo.authors)
         })
       } else {
-        console.log("Not an ISBN")
+        console.log("Not an ISBN");
         itemList.push(item);
       }
   	}
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const { join } = require("path");
+
 var app = express();
 
 var mysql = require('mysql');
@@ -6,10 +8,12 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
-connection.query('SELECT * FROM v6mbnziqjbmo11km.events', function(err, rows, fields) {
+// This is a single query
+var insertion = "INSERT INTO v6mbnziqjbmo11km.user_account (`id`, `user_name`, `password`, `email`, `day`, `time`, `content_pref`) VALUES ('5', 'user5', 'adf', 'dvxcv', '4', '10', '3')";
+connection.query("SELECT * from v6mbnziqjbmo11km.user_account", function(err, rows, fields) {
   if (err) throw err;
 
-  console.log('The solution is: ', rows[0].description);
+  console.log('The solution is: ', rows[0].user_name);
 });
 
 connection.end();
@@ -17,6 +21,10 @@ connection.end();
 var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/public'));
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
+});
+
 
 app.get('/', function(req, res) {
 	res.render('index');
